a77ac5461939b46f50bc61f9014c9e8cee5a42c6
Nihal Konda <nihal.konda@sjsu.edu>
2020-12-08 23:27:09 -0800

    Quick Commit

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export * as Helpers from './helpers';
 export * as Middlewares from './middlewares';
 export * as Repositories from './repositories';
 export * as Services from './services';
-export { Utils } from 'nk-js-library';
 export * as Test from './test/helper';
