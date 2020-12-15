939de534b21ca7a85f1222f80b5d180df3b516fd
nihalkonda5 <konda.nihal5@gmail.com>
2020-12-14 19:56:11 -0800

    Quick Commit

--- lib/config/index.d.ts
+++ lib/config/index.d.ts
@@ -1,7 +1,8 @@
 import routesList from './routes.list';
 declare const PORT: string;
+declare const CUSTOM_PORT: string;
 declare const MONGO_URI: string;
 declare const MONGO_URI_UM: string;
 declare const ACCESS_TOKEN_SECRET: string;
 declare const REFRESH_TOKEN_SECRET: string;
-export { PORT, MONGO_URI, MONGO_URI_UM, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, routesList };
+export { PORT, CUSTOM_PORT, MONGO_URI, MONGO_URI_UM, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, routesList };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,11 @@
 import routesList from './routes.list';
 
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
-} 
-  
+}
+
 const PORT = process.env.PORT;
+const CUSTOM_PORT = process.env.CUSTOM_PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const MONGO_URI_UM = process.env.MONGO_URI_UM;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
@@ -13,6 +14,7 @@ const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 export {
     PORT,
+    CUSTOM_PORT,
     MONGO_URI,
     MONGO_URI_UM,
     ACCESS_TOKEN_SECRET,
